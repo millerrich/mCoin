e00cade0bee751af1dec5b6a8235274af7768e1e
Miller Rich <william.miller.rich@gmail.com>
2020-10-10 23:49:16 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple JavaScript Blockchain
+
+## Description
+This is a simple javascript blockchain implementation with a cryptocurrency featuring Proof of Work, mining rewards and signed transactions.
---
+++ main.js
@@ -0,0 +1,20 @@
+const {Blockchain, Transaction} = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+const myKey = ec.keyFromPrivate('77009d02a3c4eb747f6631d4f3f12a1ceec812d3712c3c03ed5badec0f550050');
+const myWalletAddress = myKey.getPublic('hex');
+
+let mCoin = new Blockchain();
+
+const tx1 = new Transaction(myWalletAddress, 'public key goes here', 10);
+tx1.signTransaction(myKey);
+mCoin.addTransaction(tx1);
+
+console.log("\n Starting the miner...");
+mCoin.minePendingTransactions(myWalletAddress);
+
+
+console.log('\n Balance of miner is ', mCoin.getBalanceOfAddress(myWalletAddress));
+
+console.log('is chain valid?', mCoin.isChainValid());
